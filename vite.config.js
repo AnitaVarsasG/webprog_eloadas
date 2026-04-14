4624abcacf7de90bc78bbb0a212ba6b78c310290
AnitaVarsasG <guti.anita88@gmail.com>
2026-04-14 22:06:52 +0200

    implement calculator and tictactoe

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        
-        main: resolve(__dirname, "index.html"),
-        javascript: resolve(__dirname, "javascript.html"),
-        react: resolve(__dirname, "react.html"),
-
-        
-      },
-    },
-  },
-});
+        main: resolve(__dirname, 'index.html'),
+        javascript: resolve(__dirname, 'javascript.html'),
+        react: resolve(__dirname, 'react.html'),
+        spa: resolve(__dirname, 'spa.html') 
+      }
+    }
+  }
+})
