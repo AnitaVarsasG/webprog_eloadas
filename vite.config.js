95633636fdcf7c0137a5b29f7ba23e4f36f35ec8
danielkulcsar <60344276+danielkulcsar@users.noreply.github.com>
2026-04-14 21:18:47 +0200

    3 feladat kész

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        
+        main: resolve(__dirname, "index.html"),
+        javascript: resolve(__dirname, "javascript.html"),
+        react: resolve(__dirname, "react.html"),
+
+        
+      },
+    },
+  },
+});
